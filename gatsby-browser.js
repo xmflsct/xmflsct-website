a63c2e6028478f771c637c4a605a735c09b02896
Zhiyuan Zheng <me@xmflsct.com>
2020-09-18 00:07:43 +0200

    Smooth jump to top animation
    https://github.com/framer/motion/issues/626#issuecomment-658952546

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
-import "./src/styles/main.scss";
+import './src/styles/main.scss'
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), 350)
+  } else {
+    const savedPosition = getSavedScrollPosition(location)
+    window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), 350)
+  }
+  return false
+}
