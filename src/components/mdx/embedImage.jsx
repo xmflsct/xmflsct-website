cbb8ec391cb4a5df65a7a63378fd6a8303903305
Zhiyuan Zheng <me@xmflsct.com>
2020-09-17 21:53:04 +0200

    Adjust avatar styling

--- src/components/mdx/embedImage.jsx
+++ src/components/mdx/embedImage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-const EmbedImage = ({ children, credit, width }) => (
-  <div className={`image-fix mb-4 ${width && `lg:w-${width} lg:ml-${width}Margin`}`}>
+const EmbedImage = ({ children, classes, credit, width }) => (
+  <div
+    className={`${classes} image-fix mb-4 ${width &&
+      `lg:w-${width} lg:ml-${width}Margin`}`}
+  >
     {children}
     {credit && (
       <figcaption className='opacity-50 text-right mt-1'>{credit}</figcaption>
