8ba6e97b1bbbf257bf795caf06e894ceb1158157
Zhiyuan Zheng <me@xmflsct.com>
2020-09-19 10:26:02 +0200

    Fixed #4

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -62,11 +62,11 @@ const Cards = ({ filter }) => {
                   }
                 />
                 <Link
-                  to={node.slug}
+                  to={'/' + node.slug}
                   className='absolute inset-0 w-full h-full flex justify-center items-center border-0 bg-white bg-opacity-75 transition duration-300 ease-in-out opacity:100 lg:opacity-0 hover:opacity-100'
                 >
                   <h2 className='text-center text-lg lg:text-2xl align-middle inline-block text-light'>
-                    {node.frontmatter.title || node.slug}
+                    {node.frontmatter.title}
                     <br />
                   </h2>
                   <h3 className='absolute left-0 top-0 border border-light px-2 py-1 m-3 text-sm lg:text-base text-light'>
@@ -86,11 +86,11 @@ const Cards = ({ filter }) => {
                 }`}
               >
                 <Link
-                  to={node.slug}
+                  to={'/' + node.slug}
                   className='absolute inset-0 w-full h-full flex justify-center items-center border-0 bg-gradient-to-r from-white to-light-bg'
                 >
                   <h2 className='text-center text-lg lg:text-2xl align-middle inline-block text-light'>
-                    {node.frontmatter.title || node.slug}
+                    {node.frontmatter.title}
                     <br />
                   </h2>
                   <h3 className='absolute left-0 top-0 border border-light px-2 py-1 m-3 text-sm lg:text-base text-light'>
